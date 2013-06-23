b1f4252287807beea9a73b6c82383593c7010149
David Wolever <david@wolever.net>
2013-06-23 16:02:50 -0400

    Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,18 @@ version = "%s.%s.%s" %django_switchuser.__version__
 setup(
     name="django-switchuser",
     version=version,
+    url="https://github.com/wolever/django-switchuser",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=[
-    ],
+    license="BSD",
+    classifiers=[ x.strip() for x in """
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        Framework :: Django
+        License :: OSI Approved :: BSD License
+        Natural Language :: English
+        Operating System :: OS Independent
+        Programming Language :: Python
+    """.split("\n") if x.strip() ],
 )
