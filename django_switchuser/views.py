be05fb1bbf26acdc59fdf27b3fa22a5b0b3690b1
David Wolever <david@wolever.net>
2014-03-26 15:40:05 -0400

    A sensible redirect on logout

--- django_switchuser/views.py
+++ django_switchuser/views.py
@@ -6,18 +6,32 @@ from django.shortcuts import render
 
 log = logging.getLogger(__name__)
 
-def guess_next(request):
+def drop_subdomain(host):
+    host_parts = host.split(".")
+    if len(host_parts) > 2:
+        return host_parts[1:]
+    return host_parts
+
+def referrer_path(meta, default=None):
+    referrer = meta.get("HTTP_REFERER")
+    if not referrer:
+        return default
+    parsed = urlparse.urlsplit(referrer)
+    next_domain = drop_subdomain(parsed.netloc)
+    cur_domain = drop_subdomain(meta.get("HTTP_HOST", ""))
+    if next_domain != cur_domain:
+        return default
+    return urlparse.urlunsplit(('', '') + parsed[2:])
+
+def guess_next(request, default=None):
     if "next" in request.GET:
         return request.GET["next"]
-    referer = request.META.get("HTTP_REFERER")
-    if not referer:
-        return "/"
-    parsed = urlparse.urlsplit(referer)
-    next = urlparse.urlunsplit(('', '') + parsed[2:])
-    return next
+    if default is not None:
+        return default
+    return referrer_path(request.META, default="/")
 
-def redirect_next(request):
-    next = guess_next(request)
+def redirect_next(request, default=None):
+    next = guess_next(request, default)
     if not next.startswith("/"):
         next = "/" + next
     return HttpResponseRedirect(next)
@@ -31,6 +45,10 @@ def su_login(request):
     check_su(request)
     if request.method == "POST":
         request.su_state.set_su_user_id(request.POST["user_id"])
+        request.session["su_logout_next"] = (
+            request.GET.get("su_logout_next") or
+            referrer_path(request.META)
+        )
         return redirect_next(request)
     return render(request, "su/login.html", {
         "next": guess_next(request),
@@ -40,7 +58,8 @@ def su_logout(request):
     check_su(request)
     if request.method == "POST":
         request.su_state.clear_su()
-        return redirect_next(request)
+        default_next = request.session.pop("su_logout_next", None)
+        return redirect_next(request, default_next)
     return render(request, "su/logout.html", {
         "next": guess_next(request),
     })
