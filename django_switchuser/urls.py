b2ae779f7ff585f5715cd5643fc1738ea9bd2ec5
David Wolever <david@wolever.net>
2017-02-03 11:27:51 -0500

    Clean up urls.py

--- django_switchuser/urls.py
+++ django_switchuser/urls.py
@@ -1,18 +1,12 @@
-from django import VERSION
+from django.conf.urls import url
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import patterns
 except ImportError:
-    from django.conf.urls import url
+    patterns = lambda _, *p: p
+
 from . import views
 
-if VERSION >= (1, 10):
-    urlpatterns = [
-        url(r"^$", views.su_login, name="su-login"),
-        url(r"^logout$", views.su_logout, name="su-logout"),
-    ]
-
-else:
-    urlpatterns = patterns("",
-        url(r"^$", views.su_login, name="su-login"),
-        url(r"^logout$", views.su_logout, name="su-logout"),
-    )
+urlpatterns = patterns("", *[
+    url(r"^$", views.su_login, name="su-login"),
+    url(r"^logout$", views.su_logout, name="su-logout"),
+])
