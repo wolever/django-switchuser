203df678bbe9d9f3ffa0cc18ffbd7da819e343bd
misato <misato.okutsu@gmail.com>
2017-02-02 22:24:23 -0500

    Avoid overwriting patterns import

--- django_switchuser/urls.py
+++ django_switchuser/urls.py
@@ -1,13 +1,18 @@
-from django.conf.urls import patterns, url
-
+from django import VERSION
+try:
+    from django.conf.urls import patterns, url
+except:
+    from django.conf.urls import url
 from . import views
 
-patterns = [
-    url(r"^$", views.su_login, name="su-login"),
-    url(r"^logout$", views.su_logout, name="su-logout"),
-]
+if VERSION > (1, 10):
+    urlpatterns = [
+        url(r"^$", views.su_login, name="su-login"),
+        url(r"^logout$", views.su_logout, name="su-logout"),
+    ]
 
-try:
-    urlpatterns = patterns("", *patterns)
-except NameError:
-    urlpatterns = patterns
+else:
+    urlpatterns = patterns("",
+        url(r"^$", views.su_login, name="su-login"),
+        url(r"^logout$", views.su_logout, name="su-logout"),
+    )
