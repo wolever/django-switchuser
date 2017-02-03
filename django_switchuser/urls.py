95793124f5d487f4f1f38b21d7ea930241baec97
misato <misato.okutsu@gmail.com>
2017-02-02 22:43:50 -0500

    Check that version is at or above django 1.10.0

--- django_switchuser/urls.py
+++ django_switchuser/urls.py
@@ -1,11 +1,11 @@
 from django import VERSION
 try:
     from django.conf.urls import patterns, url
-except:
+except ImportError:
     from django.conf.urls import url
 from . import views
 
-if VERSION > (1, 10):
+if VERSION >= (1, 10):
     urlpatterns = [
         url(r"^$", views.su_login, name="su-login"),
         url(r"^logout$", views.su_logout, name="su-logout"),
